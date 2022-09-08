290a1114a146c7e4d1cef237b06196b9405ce489
Jose <jose@mail.com>
2022-09-08 10:13:51 +0200

    add section 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+@extends("layout.app")
+@section("content")
+    @include("partials.section1")
+    @include("partials.section2")
+    @include("partials.section3")
+    @include("partials.section4")
+    @include("partials.section5")
+    @include("partials.section6")
+@endsection
